0f1aeebce2e4a71b7a76dd28a325df868bafaede
Jenaro Calviño <jen.calvineo@gmail.com>
2024-10-10 10:05:31 -0300

    fixing layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def gravatar
+    hash = Digest::SHA256.hexdigest(current_user.email.downcase)
+    
+    default = current_user.avatar
+    size = 40
+
+    params = URI.encode_www_form('d' => default, 's' => size)
+    "https://www.gravatar.com/avatar/#{hash}?#{params}"
+  end
 end
